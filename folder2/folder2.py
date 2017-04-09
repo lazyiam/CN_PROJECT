92c9f7bf952d4f0f974adaa0c36bd31d92d025a5
AyushRai09 <ayushrai982@gmail.com>
2017-04-09 16:33:15 +0530

    Receiver now receives and sends acknowledgement number accordingly.

--- folder2/folder2.py
+++ folder2/folder2.py
@@ -2,24 +2,71 @@ import socket,os,datetime
 import time
 from threading import Thread
 import threading
-global modified_files
+global ACKNo, seqNoOfReceivedPacket, noOfPacketsReceived,ackSentFlag
 class Senderthread(Thread):
     def __init__(self):
         Thread.__init__(self)
-
     def run(self):
+        Thread.__init__(self)
+        host = ""
+        port = 60000 #the receiver send the acknowledgement on port 60000
+        s = socket.socket() # for TCP
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        s.connect((host, port))
+        ackSentFlag=0
+        while True:
+            if ackSentFlag:
+                print "The acknowledgement for the received packet number ", noOfPacketsReceived, "sent with acknowledgement number ",ACKNo
+                s.send(ACKNo)
+                ackSentFlag=0
+            # command = raw_input("prompt:$ ")
+            # arg=command.split()
+            # # data = s.recv(1024)
+            # if(command==""):
+            #     continue
+
+        print('Successfully get the file')
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
+        print('connection closed')
 
 class Receiverthread(Thread):
     def __init__(self):
         Thread.__init__(self)
 
     def run(self):
+        port = 30000 #the receiver receives the packet on port 30000
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        host = ""
+
+        s.bind((host, port))
+        s.listen(5)
+        conn, addr = s.accept()
+        ackSentFlag
+        while True:
+            # print 'receiving.......'
+            data=conn.recv(1024)
+            if data:
+                originalData=data
+                data=data.split('~')
+                seqNoOfReceivedPacket=int(data[1])
+                ACKNo=seqNoOfReceivedPacket + len(data[0])
+                noOfPacketsReceived+=1
+                ackSentFlag=1
+                print "Packet expecting with sequence number ",seqNoOfReceivedPacket
+                print "Received packet number ",noOfPacketsReceived, "with sequence number ",seqNoOfReceivedPacket
+                # conn.send(ACKNo)
+            # print "data:",data
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
 
 if __name__ == '__main__':
    # Declare objects of Serverthread class
-   myThreadOb1 = Serverthread()
+   myThreadOb1 = Senderthread()
 
-   myThreadOb2 = Clientthread()
+   myThreadOb2 = Receiverthread()
 
    # Start running the threads!
    myThreadOb1.start()
@@ -29,5 +76,3 @@ if __name__ == '__main__':
    # Wait for the threads to finish...
    myThreadOb1.join()
    myThreadOb2.join()
-
-   print('Main Terminating...')
