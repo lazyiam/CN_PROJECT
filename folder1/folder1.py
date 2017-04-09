9a19f1cd7b24eece877fc40b0b64477ec757ccf2
a_goyal <anuj.goyal@students.iiit.ac.in>
2017-04-09 23:09:09 +0530

    some changes

--- folder1/folder1.py
+++ folder1/folder1.py
@@ -8,37 +8,43 @@ class Senderthread(Thread): #sender is my client
         Thread.__init__(self)
 
     def run(self):
-        f = open(filename,'rw+')
-        def sendPacket(packet_number):
-            offset = packet_number*1024
-            f.seek(offset,1);
+        windowSize=5
+        # f = open(filename,'rw+')
+        def sendPacket(seq_num):
+            offset = seq_num
+            f.seek(offset,0)
+            len_seq_no = len(str(seq_num))
+            s = f.read(1024 - len_seq_no -1)
+            pac = s + "~" + str(packet_number)
+            s.send(pac)
+
         def sendWindow(win_off_pack):
-            for i in range(win_off_pack,win_off_pack+windowSize):
-                sendPacket(i)
+            temp = seq_num
+            for x in range(5):
+                sendPacket(temp)
+                temp2 = len(str(temp))
+                temp = temp + (1024-temp2)
 
 
 
 
         host = ""
         port = 30000
-        s = soc ket.socket() # for TCP
+        s = socket.socket() # for TCP
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
         s.connect((host, port))
         windowSize=raw_input("Enter window size: ")
         t_out=raw_input("Enter timeout interval: ")
         numberOfPackets=raw_input("Enter number of pacekts to be sent: ")
 
-        iteratorOnPacketNumber=0
-        while iteratorOnPacketNumber<=numberOfPackets:
-            print "Sending", iteratorOnPacketNumber "......."
-            sendPackets(iteratorOnPacketNumber)
-            iteratorOnPacketNumber+=1
-            # command = raw_input("prompt:$ ")
-            # arg=command.split()
-            # # data = s.recv(1024)
-            # if(command==""):
-            #     continue
+        # iteratorOnPacketNumber=0
+        sendWindow(0) # Sending initial window
+        tempseqnum = 0
+        while True:
+            if seqNo > tempseqnum:
+                sendPacket(seqNo)
+                iteratorOnPacketNumber+=1
+
 
         print('Successfully get the file')
         s.shutdown(socket.SHUT_RDWR)
@@ -60,10 +66,11 @@ class Receiverthread(Thread): #receiver is my server sort of......
         s.listen(5)
         conn, addr = s.accept()
         while True:
-            # print 'receiving.......'
             data=conn.recv(1024)
             originalData=data
             data=data.split()
+            ack_recv = 1
+            seqNo = int(data)
             # print "data:",data
         s.shutdown(socket.SHUT_RDWR)
         s.close()
