230dcdb290e48c915ec08fb6ba143f6b861eda5d
a_goyal <anuj.goyal@students.iiit.ac.in>
2017-04-10 01:49:04 +0530

    without timeout kuch kuch

--- folder1/folder1.py
+++ folder1/folder1.py
@@ -2,54 +2,85 @@ import socket,os,datetime
 import time
 from threading import Thread
 import threading
-global lastACKNo, seqNo, windowSize, t_out
+seqNo=0
+windowSize=5
+t_out =5
+ack_dict = {}
+# ack_recv
+senttill = 0
 class Senderthread(Thread): #sender is my client
     def __init__(self):
         Thread.__init__(self)
 
     def run(self):
-        windowSize=5
+        f = open('testfile','rb')
+        # global senttill = 0
+        # ack_dict = {}
+        # windowSize=5
         # f = open(filename,'rw+')
+        ack_recv = 0
+        # sent = []
+        # unacked = []
         def sendPacket(seq_num):
+            global senttill
+            global ack_dict
             offset = seq_num
             f.seek(offset,0)
             len_seq_no = len(str(seq_num))
             s = f.read(1024 - len_seq_no -1)
-            pac = s + "~" + str(packet_number)
-            s.send(pac)
+            # print "s=",s
+            if not s:
+                # print "here"
+                # f.close()
+                return
+            else:
+                ack_dict[str(1024 - len_seq_no -1)] = 0
+                if (offset + 1024 - len_seq_no -1) > senttill:
+                    senttill = (offset + 1024 - len_seq_no -1)
+                pac = s + "~" + str(seq_num)
+                print "pac=",pac
+                conn.send(pac)
 
-        def sendWindow(win_off_pack):
+        def sendWindow(seq_num):
             temp = seq_num
             for x in range(5):
                 sendPacket(temp)
                 temp2 = len(str(temp))
                 temp = temp + (1024-temp2)
 
-
-
-
         host = ""
         port = 30000
         s = socket.socket() # for TCP
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.connect((host, port))
+        s.bind((host, port))
+        s.listen(15)
         windowSize=raw_input("Enter window size: ")
         t_out=raw_input("Enter timeout interval: ")
         numberOfPackets=raw_input("Enter number of pacekts to be sent: ")
 
         # iteratorOnPacketNumber=0
+        conn, addr = s.accept()
         sendWindow(0) # Sending initial window
         tempseqnum = 0
         while True:
-            if seqNo > tempseqnum:
-                sendPacket(seqNo)
+            global senttill
+            global ack_dict
+            if ack_recv == 1:
+                sendPacket(senttill + 1)
                 iteratorOnPacketNumber+=1
+                ack_recv = 0
 
 
         print('Successfully get the file')
+        conn.close()
         s.shutdown(socket.SHUT_RDWR)
         s.close()
         print('connection closed')
+
+
+
+
+
 
 class Receiverthread(Thread): #receiver is my server sort of......
     def __init__(self):
@@ -59,18 +90,22 @@ class Receiverthread(Thread): #receiver is my server sort of......
     def run(self):
         port = 60000
         s = socket.socket()
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         host = ""
 
-        s.bind((host, port))
-        s.listen(5)
-        conn, addr = s.accept()
+        s.connect((host, port))
+        # s.listen(5)
+        # conn, addr = s.accept()
         while True:
-            data=conn.recv(1024)
-            originalData=data
-            data=data.split()
-            ack_recv = 1
-            seqNo = int(data)
+            data=s.recv(1024)
+            if data:
+
+                # print "data =", str(data)
+                originalData=data
+                # data=data.split()
+                ack_recv = 1
+                seqNo = int(data)
+                ack_dict[str(seqNo-1)] =1
             # print "data:",data
         s.shutdown(socket.SHUT_RDWR)
         s.close()
@@ -84,6 +119,7 @@ if __name__ == '__main__':
    # Start running the threads!
    myThreadOb1.start()
    time.sleep(10)
+
    myThreadOb2.start()
 
    # Wait for the threads to finish...
--- folder2/folder2.py
+++ folder2/folder2.py
@@ -2,7 +2,11 @@ import socket,os,datetime
 import time
 from threading import Thread
 import threading
-global ACKNo, seqNoOfReceivedPacket, noOfPacketsReceived,ackSentFlag
+ACKNo = 0
+seqNoOfReceivedPacket = 0
+noOfPacketsReceived =0
+ackSentFlag = 0
+# senttill
 class Senderthread(Thread):
     def __init__(self):
         Thread.__init__(self)
@@ -13,12 +17,17 @@ class Senderthread(Thread):
         s = socket.socket() # for TCP
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        s.connect((host, port))
-        ackSentFlag=0
+        s.bind((host, port))
+        # global ackSentFlag
+        # ackSentFlag=0
+        s.listen(5)
+        conn ,addr = s.accept()
         while True:
+            global ACKNo,noOfPacketsReceived
+            global ackSentFlag
             if ackSentFlag:
                 print "The acknowledgement for the received packet number ", noOfPacketsReceived, "sent with acknowledgement number ",ACKNo
-                s.send(ACKNo)
+                conn.send(str(ACKNo))
                 ackSentFlag=0
             # command = raw_input("prompt:$ ")
             # arg=command.split()
@@ -27,8 +36,8 @@ class Senderthread(Thread):
             #     continue
 
         print('Successfully get the file')
-        s.shutdown(socket.SHUT_RDWR)
-        s.close()
+        # s.shutdown(socket.SHUT_RDWR)
+        conn.close()
         print('connection closed')
 
 class Receiverthread(Thread):
@@ -36,30 +45,33 @@ class Receiverthread(Thread):
         Thread.__init__(self)
 
     def run(self):
+        global ackSentFlag
         port = 30000 #the receiver receives the packet on port 30000
         s = socket.socket()
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         host = ""
 
-        s.bind((host, port))
-        s.listen(5)
-        conn, addr = s.accept()
-        ackSentFlag
+        s.connect((host, port))
+        # s.listen(5)
+        # conn, addr = s.accept()
+        ackSentFlag = 0
         while True:
             # print 'receiving.......'
-            data=conn.recv(1024)
+            data=s.recv(1024)
+            print "Data received on the end of folder2.py",data
+            global ACKNo,seqNoOfReceivedPacket,noOfPacketsReceived
             if data:
                 originalData=data
                 data=data.split('~')
                 seqNoOfReceivedPacket=int(data[1])
-                ACKNo=seqNoOfReceivedPacket + len(data[0])
+                ACKNo=seqNoOfReceivedPacket + len(data[0]) + 1
                 noOfPacketsReceived+=1
                 ackSentFlag=1
                 print "Packet expecting with sequence number ",seqNoOfReceivedPacket
                 print "Received packet number ",noOfPacketsReceived, "with sequence number ",seqNoOfReceivedPacket
                 # conn.send(ACKNo)
             # print "data:",data
-        s.shutdown(socket.SHUT_RDWR)
+        # s.shutdown(socket.SHUT_RDWR)
         s.close()
 
 if __name__ == '__main__':
@@ -71,8 +83,11 @@ if __name__ == '__main__':
    # Start running the threads!
    myThreadOb1.start()
    time.sleep(10)
+
    myThreadOb2.start()
 
    # Wait for the threads to finish...
-   myThreadOb1.join()
-   myThreadOb2.join()
+   # myThreadOb1.join()
+   # myThreadOb2.join()
+
+   # print('Main Terminating...')
